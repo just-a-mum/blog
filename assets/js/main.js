61502f81d68285ef175fbd1be287d10c75a7d16a
Natalia <kaprnata@gmail.com>
2020-02-19 16:43:30 +0000

    included category to the postbox

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,7 +65,7 @@ $(".sidebar-box .btn").click(function() {
   $el = $(this);
   $p  = $el.parent();
   $up = $p.parent();
-  $ps = $up.find("div:not('.read-more')");
+  $ps = $up.find("div.article-post");
 
   // measure how tall inside should be by adding together heights of all inside paragraphs (except read-more paragraph)
   $ps.each(function() {
